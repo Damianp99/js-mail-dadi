5142f3bbd05ab41e1db4fb5540ce0966522b7f03
Damianp99 <damianperez200199@gmail.com>
2022-01-10 16:51:40 +0100

    getting started

---
+++ js/mail.js
@@ -0,0 +1,15 @@
+/*// # Mail
+Crea un array di email;
+Chiedi all’utente la sua email.
+BONUS
+Stampare i risultati in pagina oltre che in console!
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+Consigli del giorno:
+1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+3. si, ma noi cosa vogliamo fare?
+4. torniamo a scrivere in italiano
+5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+*/
